6fbae2d5832b4d195d8c04e44c93cb8b61df5e08
mksmkrch <wedetttt@gmail.com>
2023-03-13 20:28:53 +0300

    Get/Set HW

--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -2,16 +2,16 @@ public class WolfTest {
 
     public static void main(String[] args) {
         Wolf grayWolf = new Wolf();
-        grayWolf.sex = "man";
-        System.out.println("Пол: " + grayWolf.sex);
-        grayWolf.name = "Серый";
-        System.out.println("Кличка: " + grayWolf.name);
-        grayWolf.weight = 40.4f;
-        System.out.println("Вес: " + grayWolf.weight);
-        grayWolf.age = 3;
-        System.out.println("Возраст: " + grayWolf.age);
-        grayWolf.color = "Gray";
-        System.out.println("Цвет: " + grayWolf.color);
+        grayWolf.setSex("man");
+        System.out.println("Пол: " + grayWolf.getSex());
+        grayWolf.setName("Серый");
+        System.out.println("Кличка: " + grayWolf.getName());
+        grayWolf.setWeight(40.4f);
+        System.out.println("Вес: " + grayWolf.getWeight());
+        grayWolf.setAge(9);
+        System.out.println("Возраст: " + grayWolf.getAge());
+        grayWolf.setColor("Gray");
+        System.out.println("Цвет: " + grayWolf.getColor());
         grayWolf.walk();
         grayWolf.sit();
         grayWolf.run();
