1750712604d66ce7249d582475d99fd07acd1e1b
mksmkrch <wedetttt@gmail.com>
2023-04-13 17:03:18 +0300

    SQL

--- src/com/startjava/lesson_5/init_db.sql
+++ src/com/startjava/lesson_5/init_db.sql
@@ -1,11 +1,11 @@
 INSERT INTO Jaegers (modelname, mark, height, weight, status, origin, launch, kaijukill)
 VALUES ('Atlas Destroyer', 'Mark-3', 75.75, 2.120, 'destroyed', 'USA', '2015-01-30', 2),
-       ('Bracer Phoenix', 'Mark-5', 70.91, 2.128, 'destroyed', 'China', '2025-11-01', 6),
-       ('Coyote Tango', 'Mark-1', 85.34, 2.312, 'destroyed', 'Japan', '2015-12-30', 2),
-       ('Crimson Typhoon', 'Mark-4', 76.2, 1.722, 'destroyed', 'China', '2018-08-22', 7),
-       ('Gipsy Avenger', 'Mark-6', 81.77, 2.004, 'destroyed', 'France', '2034-01-12', 9),
-       ('Guardian Bravo', 'Mark-6', 73.21, 1.975, 'active', 'Spain', '2033-12-15', 2),
-       ('Romeo Blue', 'Mark-1', 77.72, 7.775, 'destroyed', 'USA', '2015-12-15', 2),
-       ('Saber Athena', 'Mark-7', 76.91, 1.628, 'destroyed', 'Australia', '2033-12-20', 1),
-       ('Storm Garuda', 'Mark-7', 60.25, 2.220, 'active', 'Indonesia', '2018-09-16', 15),
-       ('Tacit Ronin', 'Mark-1', 74.37, 7.450, 'destroyed', 'Japan', '2015-12-06', 3);
+    ('Bracer Phoenix', 'Mark-5', 70.91, 2.128, 'destroyed', 'China', '2025-11-01', 6),
+    ('Coyote Tango', 'Mark-1', 85.34, 2.312, 'destroyed', 'Japan', '2015-12-30', 2),
+    ('Crimson Typhoon', 'Mark-4', 76.2, 1.722, 'destroyed', 'China', '2018-08-22', 7),
+    ('Gipsy Avenger', 'Mark-6', 81.77, 2.004, 'destroyed', 'France', '2034-01-12', 9),
+    ('Guardian Bravo', 'Mark-6', 73.21, 1.975, 'active', 'Spain', '2033-12-15', 2),
+    ('Romeo Blue', 'Mark-1', 77.72, 7.775, 'destroyed', 'USA', '2015-12-15', 2),
+    ('Saber Athena', 'Mark-7', 76.91, 1.628, 'destroyed', 'Australia', '2033-12-20', 1),
+    ('Storm Garuda', 'Mark-7', 60.25, 2.220, 'active', 'Indonesia', '2018-09-16', 15),
+    ('Tacit Ronin', 'Mark-1', 74.37, 7.450, 'destroyed', 'Japan', '2015-12-06', 3);
--- src/com/startjava/lesson_5/queries.sql
+++ src/com/startjava/lesson_5/queries.sql
@@ -1,49 +1,51 @@
 \echo 'Robots list'
 SELECT * 
-  FROM Jaegers;
+    FROM Jaegers;
 
 \echo 'List of non-destroyed robots'
 SELECT * 
-  FROM jaegers 
- WHERE status = 'active';
+    FROM jaegers
+   WHERE status = 'active';
 
 \echo 'List of "Mark-1" and "Mark-4" series robots'
 SELECT * 
-  FROM jaegers 
- WHERE MARK IN ('Mark-1', 'Mark-4');
+    FROM jaegers
+   WHERE mark IN ('Mark-1', 'Mark-4');
 
 \echo 'List of robots excluding "Mark-1" and "Mark-4" series'
 SELECT * 
-  FROM jaegers 
- WHERE MARK NOT IN ('Mark-1', 'Mark-4');
+    FROM jaegers
+   WHERE mark NOT IN ('Mark-1', 'Mark-4');
 
 \echo 'List of robots sorted by series descending'
 SELECT * 
-  FROM jaegers 
- ORDER BY mark DESC;
+    FROM jaegers
+   ORDER BY mark DESC;
 
 \echo 'The oldest robot'
 SELECT * 
-  FROM jaegers 
- WHERE launch = 
-    (SELECT MIN(launch) 
-       FROM jaegers);
+    FROM jaegers
+   WHERE launch =
+         (SELECT MIN(launch)
+            FROM jaegers);
 
 \echo 'The robots that destroyed the most kaiju'
 SELECT * 
-  FROM jaegers 
- WHERE kaijukill > 5;
+    FROM jaegers
+   WHERE kaijukill IN
+         (SELECT MAX(kaijukill)
+            FROM jaegers);
 
 \echo 'Average weight of robots'
 SELECT AVG(weight) 
-  FROM jaegers;
+    FROM jaegers;
 
 \echo '+1 kaiju kill to all surviving robots'
 UPDATE jaegers 
-   SET kaijukill = kaijukill + 1 
- WHERE status = 'active';
+    SET kaijukill = kaijukill + 1
+  WHERE status = 'active';
  
 \echo 'Removing destroyed robots'
 DELETE 
-  FROM jaegers 
- WHERE status = 'destroyed';
+    FROM jaegers
+   WHERE status = 'destroyed';
